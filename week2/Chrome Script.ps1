9c87c996309a634893c3126a9c65fd9cb8749844
SleepyDream <reece.hoskyns@mymail.champlain.edu>
2024-01-26 10:44:25 -0500

    Week 2 Powershell Scritps

---
+++ week2/Chrome Script.ps1
@@ -0,0 +1,10 @@
+﻿$chromeProcess = Get-Process -Name chrome -ErrorAction SilentlyContinue
+
+if ($chromeProcess) {
+    echo "Closing Chrome"
+    $chromeProcess | ForEach-Object { $_.CloseMainWindow() | Out-Null }
+    $chromeProcess | ForEach-Object { $_.WaitForExit(5000) | Out-Null }
+}else{
+    echo "Opening Chrome"
+    Start-Process "chrome.exe" -ArgumentList "https://www.champlain.edu"
+}
---
+++ week2/Save Stopped Services.ps1
@@ -0,0 +1,5 @@
+﻿$stoppedServices = Get-Service | Where-Object { $_.Status -eq 'Stopped' }
+
+$sortedServices = $stoppedServices | Sort-Object -Property Name
+
+$sortedServices | Export-Csv -Path 'C:\Users\champuser\SYS320-01\week2\output.csv' -NoTypeInformation
