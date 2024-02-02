c2ab3d4ecd2483df5746000909d4f2fffa73e891
SleepyDream <reece.hoskyns@mymail.champlain.edu>
2024-02-02 12:31:15 -0500

    Week 3 files

---
+++ week3/Dot Notation.ps1
@@ -0,0 +1,12 @@
+﻿. (Join-Path $PSScriptRoot Event_logging.ps1)
+
+clear
+
+$loginoutTable = getLoginouts(15)
+$loginoutTable
+
+$startupTable = getPowerOn(15)
+$startupTable
+
+$shutdownTable = getPowerOff(15)
+$shutdownTable
--- week3/Event_logging.ps1
+++ week3/Event_logging.ps1
@@ -1,20 +1,58 @@
-$loginouts = Get-EventLog system -Source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-14)
+function getLoginouts($days){
+
+$loginoutslogs = Get-EventLog system -Source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-$days)
 
 $loginoutsTable = @()
-for($i=0; $i -lt $loginouts.Count; $i++) {
+for($i=0; $i -lt $loginoutslogs.Count; $i++) {
     $event = ""
-    if($loginouts[$i].EventID -eq 7001) {$event = "Logon"}
-    if($loginouts[$i].EventID -eq 7002) {$event = "Logoff"}
+    if($loginoutslogs[$i].EventID -eq 7001) {$event = "Logon"}
+    if($loginoutslogs[$i].EventID -eq 7002) {$event = "Logoff"}
 
-    $user = $loginouts[$i].ReplacementStrings[5]
+    $userPID = New-Object System.Security.Principal.SecurityIdentifier($loginoutslogs[$i].ReplacementStrings[1])
+    $user = $userPID.Translate([System.Security.Principal.NTAccount])
 
     $loginoutsTable += [PSCustomObject]@{
-    Time = $loginouts[$i].TimeGenerated;
-    ID = $loginouts[$i].EventID;
-    Event = $event
-    User = $user
-
+    "Time" = $loginoutslogs[$i].TimeGenerated;
+    "ID" = $loginoutslogs[$i].EventID;
+    "Event"  = $event
+    "User" = $user.Value
     }
 }
+return $loginoutsTable
+}
 
-$loginoutsTable
+
+function getPowerOn($days){
+
+$poweronlogs = Get-EventLog system -After (Get-Date).AddDays(-$days) | Where-Object{$_.EventID -eq "6005"}
+
+$poweronTable = @()
+for($i=0; $i -lt $poweronlogs.Count; $i++) {
+    $poweronTable += [PSCustomObject]@{
+    "Time" = $poweronlogs[$i].TimeGenerated;
+    "ID" = $poweronlogs[$i].EventID; 
+    "Event" = "Startup"  
+    "User" = "System"; 
+    }
+}
+return $poweronTable
+
+}
+
+
+function getPowerOff($days){
+
+$powerofflogs = Get-EventLog system -After (Get-Date).AddDays(-$days) | Where-Object{$_.EventID -eq "6006"}
+
+$poweroffTable = @()
+for($i=0; $i -lt $powerofflogs.Count; $i++) {
+    $poweroffTable += [PSCustomObject]@{
+    "Time" = $powerofflogs[$i].TimeGenerated;
+    "ID" = $powerofflogs[$i].EventID; 
+    "Event" = "Shutdown"  
+    "User" = "System"; 
+    }
+}
+return $poweroffTable
+
+}
