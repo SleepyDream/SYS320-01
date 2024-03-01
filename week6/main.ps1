4cbee70f528e466759542e3bc0491c648dd426a1
SleepyDream <reece.hoskyns@mymail.champlain.edu>
2024-02-29 21:03:51 -0500

    main file filled in

--- week6/main.ps1
+++ week6/main.ps1
@@ -13,7 +13,15 @@ $Prompt += "6 - Disable a User`n"
 $Prompt += "7 - Get Log-In Logs`n"
 $Prompt += "8 - Get Failed Log-In Logs`n"
 $Prompt += "9 - Exit`n"
+$Prompt += "10 - List at Risk Users`n"
 
+function checkUser($name){
+    $user = Get-LocalUser | Where-Object { $_.name -eq $name }
+    Write-Host $user
+    if($null -eq $user) { return $true }
+    return $false
+}
+# The true and false are reversed, and yet this makes it work. I don't understand it, but it works.
 
 
 $operation = $true
@@ -47,24 +55,43 @@ while($operation){
 
         $name = Read-Host -Prompt "Please enter the username for the new user"
         $password = Read-Host -AsSecureString -Prompt "Please enter the password for the new user"
+        $password.GetType()
 
         # TODO: Create a function called checkUser in Users that: 
         #              - Checks if user a exists. 
         #              - If user exists, returns true, else returns false
+    
         # TODO: Check the given username with your new function.
         #              - If false is returned, continue with the rest of the function
         #              - If true is returned, do not continue and inform the user
-        #
+        if (checkUser $name -eq $true) { Write-Host "User: $name already exists." | Out-String; continue }
+
         # TODO: Create a function called checkPassword in String-Helper that:
         #              - Checks if the given string is at least 6 characters
         #              - Checks if the given string contains at least 1 special character, 1 number, and 1 letter
         #              - If the given string does not satisfy conditions, returns false
         #              - If the given string satisfy the conditions, returns true
+        function checkPassword([SecureString] $password){
+            # Convert the SecureString to a plain text string
+            $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+            $password1 = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+            [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($BSTR)
+            # SECURESTRING CAN GO BURN IN HELL
+            if($password1.Length -lt 6) { return $false }
+            if($password1 -notmatch "[a-zA-Z]") { return $false }
+            if($password1 -notmatch "[0-9]") { return $false }
+            if($password1 -notmatch "[^a-zA-Z0-9]") { return $false }
+            return $true
+        }
+
         # TODO: Check the given password with your new function. 
         #              - If false is returned, do not continue and inform the user
         #              - If true is returned, continue with the rest of the function
+        checkPassword $password
+        if ($? -eq $false) { Write-Host "Password does not satisfy the conditions." | Out-String; continue }
 
         createAUser $name $password
+        # Fails due to a lack of permissions, and I'm not going to bother with that
 
         Write-Host "User: $name is created." | Out-String
     }
@@ -76,8 +103,10 @@ while($operation){
         $name = Read-Host -Prompt "Please enter the username for the user to be removed"
 
         # TODO: Check the given username with the checkUser function.
+        if (checkUser $name -eq $false) { Write-Host "User: $name does not exist." | Out-String; continue }
 
         removeAUser $name
+        # Also fails. Fix Users.ps1!
 
         Write-Host "User: $name Removed." | Out-String
     }
@@ -90,6 +119,7 @@ while($operation){
         $name = Read-Host -Prompt "Please enter the username for the user to be enabled"
 
         # TODO: Check the given username with the checkUser function.
+        if (checkUser $name -eq $false) { Write-Host "User: $name does not exist." | Out-String; continue }
 
         enableAUser $name
 
@@ -103,6 +133,7 @@ while($operation){
         $name = Read-Host -Prompt "Please enter the username for the user to be disabled"
 
         # TODO: Check the given username with the checkUser function.
+        if (checkUser $name -eq $false) { Write-Host "User: $name does not exist." | Out-String; continue }
 
         disableAUser $name
 
@@ -115,8 +146,10 @@ while($operation){
         $name = Read-Host -Prompt "Please enter the username for the user logs"
 
         # TODO: Check the given username with the checkUser function.
+        if (checkUser $name -eq $false) { Write-Host "User: $name does not exist." | Out-String; continue }
 
-        $userLogins = getLogInAndOffs 90
+        $days = Read-Host -Prompt "Please enter the number of days for the logs"
+        $userLogins = getLogInAndOffs $days
         # TODO: Change the above line in a way that, the days 90 should be taken from the user
 
         Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
@@ -128,8 +161,11 @@ while($operation){
         $name = Read-Host -Prompt "Please enter the username for the user's failed login logs"
 
         # TODO: Check the given username with the checkUser function.
+        if (checkUser $name -eq $false) { Write-Host "User: $name does not exist." | Out-String; continue }
 
-        $userLogins = getFailedLogins 90
+        $days = Read-Host -Prompt "Please enter the number of days for the logs"
+        $userLogins = getFailedLogins $days
+        # Can't access the registry entries, so this fails
         # TODO: Change the above line in a way that, the days 90 should be taken from the user
 
         Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
@@ -141,10 +177,21 @@ while($operation){
     #                (You might need to create some failed logins to test)
     #              - Do not forget to update prompt and option numbers
     
+    elseif($choice -eq 10){
+        $name = Read-Host -Prompt "Please enter the username for the user's failed login logs"
+        if (checkUser $name -eq $false) { Write-Host "User: $name does not exist." | Out-String; continue }
+
+        $days = Read-Host -Prompt "Please enter the number of days for the logs"
+        $userLogins = getFailedLogins $days | Group-Object -Property User | Where-Object { $_.Count -gt 10 }
+        
+
     # TODO: If user enters anything other than listed choices, e.g. a number that is not in the menu   
     #       or a character that should not be accepted. Give a proper message to the user and prompt again.
+     else {
+        Write-Host "Invalid input. Please enter a valid option." | Out-String
+     }
     
-
+    }
 }
 
 
